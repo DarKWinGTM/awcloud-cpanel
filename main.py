28e4ad5f619f27b9f7ec733009778b3fed158532
Patiwat Numbut <tonmaster.1.08@hotmail.com>
2022-02-14 06:23:22 +0000

    v0.0151.637.70

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import os
-import re
-import sys
-import glob
-import time
-import json
 import pytz
-import uuid
-import base64
-import random
-import logging
-import _thread
 import datetime
-import resource
-#   import requests
-import grequests
-import collections
-#   https://pypi.org/project/cloudscraper/
-import cloudscraper
-from replit import db
-from cryptography.fernet import Fernet
-from inspect import currentframe as SourceCodeLine
-from flask import Flask, json, jsonify, request, redirect, session, send_from_directory, Response, make_response, render_template
 import gc
 
 timedate = datetime.datetime

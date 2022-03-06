1491156051c595310bacdcd0a644cdad15fe7614
Patiwat Numbut <tonmaster.1.08@hotmail.com>
2022-03-06 10:55:00 +0000

    v0.0151.637.82 #7

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ import gc
 timedate = datetime.datetime
 timezone = pytz.timezone
 
-os.popen("rm -f .git/index.lock; git fetch --all; git reset --hard; git reset --hard origin/master; git stash save ''; git pull --force; ")
+os.popen("rm -f .git/index.lock; rm -f .git/refs/heads/master.lock; rm -f .git/HEAD.lock; rm -f .git/ORIG_HEAD.lock; git fetch --all; git reset --hard; git reset --hard origin/master; git stash save ''; git pull --force; ")

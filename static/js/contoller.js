e4ad708c96f0f99460b1bf29d8e8fe26ccb2a5d4
Patiwat Numbut <tonmaster.1.08@hotmail.com>
2022-05-19 10:24:49 +0000

    v0.0151.637.102 #10

--- static/js/contoller.js
+++ static/js/contoller.js
@@ -1032,7 +1032,7 @@ $(document).ready(function() {
 									'th[id*="' + this['var']['id'] + '-fw-panel-monitor"]'
 								).querySelector(
 									'div.fw-withdraw-fmc-text'
-								).innerText = 'WITHDRAW FWG ' + ('0000' + this['var']['db']['value']).slice(-'0000'.length); 
+								).innerText = 'WITHDRAW FWC ' + ('0000' + this['var']['db']['value']).slice(-'0000'.length); 
 								
 								fetch(
 									`/vers/fw/set?waxid=${ this['var']['id'] }&auto_with_fmc=${ this['var']['db']['check'] }` + 

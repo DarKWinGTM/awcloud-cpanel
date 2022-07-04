846eb48c29f36494156af40f5b3eadbef81a2ddf
Patiwat Numbut <tonmaster.1.08@hotmail.com>
2022-07-04 18:07:18 +0000

    v0.0151.637.104 #27

--- static/js/contoller.js
+++ static/js/contoller.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-    //	window.onload = function (){
+    //	window.onload = function (){ 
         
         window['waxserver'] = {}; window['waxserver']['endpoint'] = [
 			//	{
